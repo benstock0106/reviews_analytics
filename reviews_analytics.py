938ec146e3d4b88996afd94b7645baefce6b461f
Ben <yurun0106@gmail.com>
2022-03-16 13:55:30 +0800

    new version

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -1,6 +1,6 @@
 data = []
 count = 0
-with open('reviews.txt', 'r') as f:
+with open('reviews.txt', 'r') as f: #'r'是讀取的意思
     for line in f:
         data.append(line)
         count += 1 #count = count + 1的快寫法
@@ -12,3 +12,10 @@ sum_len = 0
 for d in data:
     sum_len += len(d) #算出100萬筆留言的總長度
 print('平均是' ,sum_len/len(data)) #總長度 除 100萬筆留言 求得留言平均長度
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
